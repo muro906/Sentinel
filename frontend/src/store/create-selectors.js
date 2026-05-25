440ac1131bf1c0029803f60009c84329c9acbdc0
muro906 <mainamillicent68@gmail.com>
2026-05-25 15:42:34 +0300

    create a function that automatically generates selectors for stores

---
+++ frontend/src/store/create-selectors.js
@@ -0,0 +1,8 @@
+const createSelectors = (_store) =>{
+    const store = _store;
+    store.use = {};
+    for (const k of Object.keys(store.getState())){
+        store.use[k] = () => store((state)=> state[k]);
+    }
+    return store;
+}
